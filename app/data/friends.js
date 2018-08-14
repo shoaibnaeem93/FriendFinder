a80fa1ce4654bbca1672c15f9aa78b0896237804
Shoaib Naeem <you@example.com>
2018-08-14 13:16:34 -0700

    adding to heroku

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+    {
+        name: "Earth Bender",
+        pic: "https://i.pinimg.com/474x/d6/10/8c/d6108c06a99a30c86e6fb6b5bc2714a9--halloween-cosplay-halloween-costumes.jpg",
+        scores: [
+            "1",
+            "3", 
+            "4", 
+            "3", 
+            "5", 
+            "4", 
+            "4", 
+            "2",
+            "2", 
+            "1"
+        ]
+    },
+    {
+        name: "Water Bender",
+        pic: "https://orig00.deviantart.net/569f/f/2012/150/3/2/katara_by_jdeberge-d51ni05.jpg",
+        scores: [
+            "5",
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "5", 
+            "3", 
+            "4",
+            "3", 
+            "1"
+        ]
+    },
+    {
+        name: "Air Bender",
+        pic: "http://cdn-static.denofgeek.com/sites/denofgeek/files/9/21//the-last-airbender-aang-the-avatar.jpg",
+        scores: [
+            "5",
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "5", 
+            "5", 
+            "2",
+            "1", 
+            "4"
+        ]
+    },
+    {
+        name: "Fire Bender",
+        pic: "https://banner2.kisspng.com/20180401/aew/kisspng-zuko-aang-cosplay-costume-nickelodeon-aang-5ac1849e840aa4.2957715015226318385409.jpg",
+        scores: [
+            "5",
+            "5", 
+            "5", 
+            "5", 
+            "1", 
+            "5", 
+            "1", 
+            "1",
+            "1", 
+            "1"
+        ]
+    },
+    ];
+    
+    module.exports = friends;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+// =============================================================
+var path = require('path');
+
+// Routing
+// =============================================================
+module.exports = function(app){
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+  
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+};
